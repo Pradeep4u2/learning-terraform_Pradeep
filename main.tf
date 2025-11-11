d33419230de77dd40d379ff2846f6297b33c0465
Pradeep4u2 <128627674+Pradeep4u2@users.noreply.github.com>
2025-11-11 16:25:40 -0500

    Refactor AWS resources for web_test instance

--- main.tf
+++ main.tf
@@ -18,18 +18,18 @@ data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_instance" "blog" {
+resource "aws_instance" "web_test" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [aws_security_group.web_test.id]
 
   tags = {
-    Name = "Learning Terraform"
+    Name = "Learning Terraform_Test"
   }
 }
 
-resource "aws_security_group" "blog" {
-  name        = "blog"
+resource "aws_security_group" "web_test" {
+  name        = "web_test"
   description = "Allow http and https in. Allow Everything out"
   tags = {
     Terraform = "true"
@@ -37,31 +37,31 @@ resource "aws_security_group" "blog" {
   vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_security_group_rule" "blog_http_in" {
+resource "aws_security_group_rule" "web_test_http_in" {
   type        = "ingress"
   from_port   = 80
   to_port     = 80
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.blog.id
+  security_group_id = aws_security_group.web_test.id
 }
 
 
-resource "aws_security_group_rule" "blog_https_in" {
+resource "aws_security_group_rule" "web_test_https_in" {
   type        = "ingress"
   from_port   = 443
   to_port     = 443
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.blog.id
+  security_group_id = aws_security_group.web_test.id
 }
 
 
-resource "aws_security_group_rule" "blog_everything_out" {
+resource "aws_security_group_rule" "web_test_everything_out" {
   type        = "egress"
   from_port   = 0
   to_port     = 0
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.blog.id
+  security_group_id = aws_security_group.web_test.id
 }
