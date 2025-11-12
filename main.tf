a3383e10d017de50e8c611d87c6dc6bff92cd859
Pradeep4u2 <128627674+Pradeep4u2@users.noreply.github.com>
2025-11-12 14:49:43 -0500

    updated listeners

--- main.tf
+++ main.tf
@@ -64,13 +64,15 @@ module "blog_alb" {
   subnets = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
 
-  target_groups = {
+  target_groups = [
+  {
       name_prefix      = "blog"
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
       target_id        = "aws_instance.blog.id"
     }
+  ]
 
     http_tcp_listeners = [
       {
