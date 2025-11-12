f3c10d57d798c699fe9163f403cb5d0af27fb4ef
Pradeep4u2 <128627674+Pradeep4u2@users.noreply.github.com>
2025-11-12 15:38:04 -0500

    code from Registry

--- main.tf
+++ main.tf
@@ -64,13 +64,26 @@ module "alb" {
   subnets = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
 
-  listeners = {
-    http = {
+listeners = {
+    ex-http-https-redirect = {
       port     = 80
       protocol = "HTTP"
-      target_group_index = 0
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
       }
     }
+    ex-https = {
+      port            = 443
+      protocol        = "HTTPS"
+      certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
+
+      forward = {
+        target_group_key = "my_target"
+      }
+    }
+  }
 
   target_groups = {
     my_target = {
