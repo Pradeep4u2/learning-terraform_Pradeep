d5e6b16700e8a00c64ee39fb0df673cb526a4274
Pradeep4u2 <128627674+Pradeep4u2@users.noreply.github.com>
2025-11-14 15:31:14 -0500

    target ID is updated

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -64,7 +64,7 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id        = "module.blog_alb.target_id"
+      target_id        = module.blog_alb.target_id
     }
   }
 
