3b9b99ba60d6e6536d9584a7d6403735edc9cdba
Pradeep4u2 <128627674+Pradeep4u2@users.noreply.github.com>
2025-11-14 09:05:59 -0500

    target group arns fix

--- main.tf
+++ main.tf
@@ -42,8 +42,8 @@ module "autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  security_groups     = [module.blog_sg.security_group_id]
-  target_group_arns  = [module.blog_alb.target_group_arns["my_target"]]
+  security_groups     = module.blog_sg.security_group_id
+  target_group_arns  = target_group_arns = module.blog_alb.target_group_arns
 
   image_id      = data.aws_ami.app_ami.id
   instance_type = var.instance_type
