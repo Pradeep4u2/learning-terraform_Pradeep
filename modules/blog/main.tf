54bd7c39595e0f6020747e16cc8a925c861b52cd
Pradeep4u2 <128627674+Pradeep4u2@users.noreply.github.com>
2025-11-14 22:03:29 -0500

    removed module Ec2

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -47,19 +47,6 @@ module "blog_autoscaling" {
 
 }
 
-module "blog_ec2" {
-
-  count = var.instance_count
-  ami           = var.ami
-  instance_type = var.instance_type
-  subnet_id     = element(module.blog_vpc.public_subnets, count.index)
-  security_groups = [module.blog_sg.security_group_id]
-}
-
-output "instance_ids" {
-  value = aws_instance.blog[*].id
-}
-
 
 module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
